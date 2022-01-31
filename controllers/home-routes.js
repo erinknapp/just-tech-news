1c059827cc4f54bc23817092b5551fc871630853
Erin Knapp <erin.knapp@gmail.com>
2022-01-31 17:49:54 -0500

    homepage finished

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,10 @@
+const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const posts = dbPostData.map(post => post.get({ plain: true }));
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -27,7 +29,8 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      // pass a single post object into the homepage template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
       res.render('homepage', { posts });
     })
     .catch(err => {
@@ -35,3 +38,5 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+module.exports = router;
