07dab3e30e90b6a4427061efc7486f394be23f4f
Erin Knapp <erin.knapp@gmail.com>
2022-02-04 19:51:40 -0500

    worked on handlebars templating and conditionals

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -38,7 +38,11 @@ router.get('/:id', (req, res) => {
         through: Vote,
         as: 'voted_posts'
       }
-    ]
+    ],
+    res.render('single-post', {
+      post,
+      loggedIn: req.session.loggedIn
+    })
   })
     .then(dbUserData => {
       if (!dbUserData) {
@@ -61,23 +65,22 @@ router.post('/', (req, res) => {
     password: req.body.password
   })
     .then(dbUserData => {
-      res.json(dbUserData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    }).then(dbUserData => {
       req.session.save(() => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
-    
+  
         res.json(dbUserData);
       });
     })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
@@ -96,11 +99,10 @@ router.post('/login', (req, res) => {
     }
 
     req.session.save(() => {
-      // declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
-
+  
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
